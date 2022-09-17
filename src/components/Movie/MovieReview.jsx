8b5f076ba3147c9512c8a47b99903e55064a4263
Samantha <samanthali7@outlook.com>
2022-09-17 19:03:03 +0200

    final git

--- src/components/Movie/MovieReview.jsx
+++ src/components/Movie/MovieReview.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Review from "./Review"
 
 const client = axios.create({
-    baseURL: "http://localhost:5000/movieRecommend" 
+    baseURL: "/movieRecommend" 
   });
 
 
