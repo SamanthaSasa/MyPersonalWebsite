bba2cd18977f7f551d662c7387b3811f31da79dd
Samantha <samanthali7@outlook.com>
2023-04-20 22:13:14 +0200

    modify some typo

--- src/components/Engineer/MyJourney.jsx
+++ src/components/Engineer/MyJourney.jsx
@@ -6,14 +6,14 @@ const educations = [
     {"university":"KU Leuven",
     "location":"Leuven, Belgium",
     "time":"Sep. 2021-present",
-    "major":"Bachelor of Engineering Electromechanical",
+    "major":"Bachelor in Electromechanical Engineering",
     "logo":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/KU_Leuven_logo.svg/1200px-KU_Leuven_logo.svg.png",
     "website":"https://www.kuleuven.be/english/kuleuven/"
 },
     {"university":"Wuhan University of Technology",
     "location":"Wuhan, China",
     "time":"Sep. 2019-2021",
-    "major":"Bachelor of Engineering in Automotive Engineering",
+    "major":"Bachelor in Automotive Engineering",
     "logo":"https://www.eastjuntai.com/uploads/image/20220111/17/wuhan-university-of-technology-campus7.jpg",
     "website":"http://english.whut.edu.cn/"
 }];
